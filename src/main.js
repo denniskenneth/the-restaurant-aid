71594eb01d2f05a52af1e5a07e4e669efb9a40b2
Dennis Kenneth <dennyify123@gmail.com>
2020-12-08 22:08:06 +0000

    deploy

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import apollo from "./apollo";
+import store from "./store";
+import router from "./router";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.prototype.$apollo = apollo;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
